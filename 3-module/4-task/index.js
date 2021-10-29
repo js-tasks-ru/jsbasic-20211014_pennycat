f2e617ac7858552687a000521ba29cb2518d57a6
Юлия Дмитриева <pennycat@yandex.ru>
2021-10-30 01:13:54 +0300

    Решение задач - module 3 (1-5 tasks), module 4 (1,2 tasks)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,16 @@
 function showSalary(users, age) {
-  // ваш код...
+  let usersString = '';
+  let newUsers = users.filter(user => user.age <= age);
+  let last = newUsers.length - 1;
+  newUsers.forEach((newUser, i) =>{
+    if(i == last){
+      usersString += `${newUser.name}, ${newUser.balance}`;
+    } else {
+      usersString += `${newUser.name}, ${newUser.balance}\n`;
+    }
+  });
+  return usersString;
 }
+
+
+
